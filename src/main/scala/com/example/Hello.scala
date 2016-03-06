2923139db0210148ee178441b87a64b23f9faaad
shigemk2 <i.am.shige@gmail.com>
2016-03-06 21:18:59 +0900

    Parse cron definition

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -1,38 +1,32 @@
 package com.example
 
 import java.lang.InterruptedException
-import java.util.Date
+import java.util.{Locale, Date}
 
+import com.cronutils.descriptor.CronDescriptor
 import com.cronutils.mapper.CronMapper
+import com.cronutils.parser.CronParser
+import com.cronutils.model.CronType
 import com.cronutils.model.definition.CronDefinitionBuilder
 import it.sauronsoftware.cron4j._
 
 object Hello {
   def main(args: Array[String]): Unit = {
-    val cronDefinition =
-      CronDefinitionBuilder.defineCron()
-      .withSeconds().and()
-      .withMinutes().and()
-      .withHours().and()
-      .withDayOfMonth()
-      .supportsHash().supportsL().supportsW().and()
-      .withMonth().and()
-      .withDayOfWeek()
-      .withIntMapping(7, 0)
-      .supportsHash().supportsL().supportsW().and()
-      .withYear().and()
-      .lastFieldOptional()
-      .instance()
-    println(cronDefinition)
-    try {
-      val scheduler = new Scheduler()
-      scheduler.schedule("* * * * *", new CronTask())
-      scheduler.start()
-    } catch {
-      case ex: InterruptedException => {
-        ex.printStackTrace()
-      }
-    }
+    val descriptor = CronDescriptor.instance(Locale.UK)
+    val cronDefinition =  CronDefinitionBuilder.instanceDefinitionFor(CronType.CRON4J)
+    val cron4jParser= new CronParser(cronDefinition)
+    println(descriptor.describe(cron4jParser.parse("/2 * * * *")))
+    // try {
+    //   val scheduler = new Scheduler()
+    //   scheduler.schedule("* * * * *", new CronTask())
+    //   scheduler.start()
+    // } catch {
+    //   case ex: InterruptedException => {
+    //     ex.printStackTrace()
+    //   }
+    // }
+
+    // debris of cronMapper
     // val cronMapper = new CronMapper(
     //   cronDefinition,
     //   CronDefinitionBuilder.instanceDefinitionFor()
