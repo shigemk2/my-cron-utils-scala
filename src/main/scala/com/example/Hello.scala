cda9d06c39f24b3856a279d994140992095c61db
shigemk2 <i.am.shige@gmail.com>
2016-03-06 19:50:23 +0900

    Add definition

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -1,7 +1,24 @@
 package com.example
 
+import com.cronutils.model.definition.CronDefinitionBuilder
+
 object Hello {
   def main(args: Array[String]): Unit = {
+    val cronDefinition =
+      CronDefinitionBuilder.defineCron()
+      .withSeconds().and()
+      .withMinutes().and()
+      .withHours().and()
+      .withDayOfMonth()
+      .supportsHash().supportsL().supportsW().and()
+      .withMonth().and()
+      .withDayOfWeek()
+      .withIntMapping(7, 0)
+      .supportsHash().supportsL().supportsW().and()
+      .withYear().and()
+      .lastFieldOptional()
+      .instance()
+    println(cronDefinition)
     println("Hello, world!")
   }
 }
