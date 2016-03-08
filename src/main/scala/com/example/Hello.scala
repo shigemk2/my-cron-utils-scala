38c59de9f3d0f2db6f5beecf4ce1200c80ed5234
shigemk2 <i.am.shige@gmail.com>
2016-03-08 20:04:02 +0900

    Add cronfield

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -1,6 +1,7 @@
 package com.example
 
-import com.cronutils.model.CronType
+import com.cronutils.builder.model.CronField
+import com.cronutils.model.{Cron, CronType}
 import com.cronutils.model.definition.CronDefinitionBuilder
 import com.cronutils.model.time.ExecutionTime
 import com.cronutils.parser.CronParser
@@ -11,7 +12,9 @@ object Hello {
     val cronDefinition = CronDefinitionBuilder.instanceDefinitionFor(CronType.CRON4J)
     val parser = new CronParser(cronDefinition)
     val now = DateTime.now
-    val executionTime = ExecutionTime.forCron(parser.parse("0 21 * * *"))
+    val cronField = CronField
+    val cron = new Cron(cronDefinition, java.util.List[CronField])
+    val executionTime = ExecutionTime.forCron(parser.parse("49 * * * *"))
     println(executionTime.lastExecution(now))
     println(executionTime.timeFromLastExecution(now))
     println(executionTime.timeToNextExecution(now).getStandardMinutes) // returns minutes to next cron
