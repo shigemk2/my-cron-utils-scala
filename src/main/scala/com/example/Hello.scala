9981c12f3b7581b2bafc3797fc63b3b491956918
shigemk2 <i.am.shige@gmail.com>
2016-03-09 15:21:48 +0900

    Change CRON4J to QUARTZ

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -1,7 +1,6 @@
 package com.example
 
-import com.cronutils.builder.model.CronField
-import com.cronutils.model.{Cron, CronType}
+import com.cronutils.model.CronType
 import com.cronutils.model.definition.CronDefinitionBuilder
 import com.cronutils.model.time.ExecutionTime
 import com.cronutils.parser.CronParser
@@ -9,12 +8,12 @@ import org.joda.time.DateTime
 
 object Hello {
   def main(args: Array[String]): Unit = {
-    val cronDefinition = CronDefinitionBuilder.instanceDefinitionFor(CronType.CRON4J)
+    val cronDefinition = CronDefinitionBuilder.instanceDefinitionFor(CronType.QUARTZ)
     val parser = new CronParser(cronDefinition)
     val now = DateTime.now
-    val cronField = CronField
-    val cron = new Cron(cronDefinition, java.util.List[CronField])
-    val executionTime = ExecutionTime.forCron(parser.parse("49 * * * *"))
+    // val cronField = CronField
+    // val cron = new Cron(cronDefinition, java.util.List[CronField])
+    val executionTime = ExecutionTime.forCron(parser.parse("? 49 * * * ? 2016"))
     println(executionTime.lastExecution(now))
     println(executionTime.timeFromLastExecution(now))
     println(executionTime.timeToNextExecution(now).getStandardMinutes) // returns minutes to next cron
