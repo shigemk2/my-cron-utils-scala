8e5833baf57a1d07f1f8a147cb1085aad5156b33
shigemk2 <i.am.shige@gmail.com>
2016-03-17 12:21:19 +0900

    Fix

--- build.sbt
+++ build.sbt
@@ -6,7 +6,8 @@ scalaVersion := "2.11.7"
 
 // Change this to another test framework if you prefer
 libraryDependencies ++= Seq(
-  "it.sauronsoftware.cron4j" % "cron4j" % "2.2.5",
+  "joda-time"         % "joda-time"           % "2.1",
+"it.sauronsoftware.cron4j" % "cron4j" % "2.2.5",
   "com.cronutils" % "cron-utils" % "3.1.5",
   "org.scalatest" %% "scalatest" % "2.2.4" % "test"
 )
--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -1,8 +1,10 @@
 package com.example
 
+import java.util.Locale
+
+import com.cronutils.descriptor.CronDescriptor
 import com.cronutils.model.CronType
 import com.cronutils.model.definition.CronDefinitionBuilder
-import com.cronutils.model.time.ExecutionTime
 import com.cronutils.parser.CronParser
 import org.joda.time.DateTime
 
@@ -10,12 +12,9 @@ object Hello {
   def main(args: Array[String]): Unit = {
     val cronDefinition = CronDefinitionBuilder.instanceDefinitionFor(CronType.QUARTZ)
     val parser = new CronParser(cronDefinition)
-    val now = DateTime.now
-    // val cronField = CronField
-    // val cron = new Cron(cronDefinition, java.util.List[CronField])
-    val executionTime = ExecutionTime.forCron(parser.parse("? 49 * * * ? 2016"))
-    println(executionTime.lastExecution(now))
-    println(executionTime.timeFromLastExecution(now))
-    println(executionTime.timeToNextExecution(now).getStandardMinutes) // returns minutes to next cron
+    val now = DateTime.now()
+    val descriptor = CronDescriptor.instance(Locale.UK)
+    // val executionTime = ExecutionTime.forCron(parser.parse("* 49 * * * ? 2016"))
+    println(descriptor.describe(parser.parse("* * * * *")))
   }
 }
